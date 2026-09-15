41d9ea33ebe3b3b2a798ded52f1c445855e76759
Rafae Kamran <abkamran@davidson.edu>
2026-09-15 14:39:03 -0400

    Implement heuristic h1 for misplaced tiles count
    Add heuristic function h1 to count misplaced tiles.

--- src/astar.py
+++ src/astar.py
@@ -1,1 +1,15 @@
+#h1 Misplaced tiles
 
+def h1 (board):
+
+  count = 0
+  for i in range (9):
+
+    if board.state[i] != 0 and board.state[i] != GOAL[i]:
+
+      count += 1
+  
+  return count
+
+#h2
+#A*
