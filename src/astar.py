116a024c04b9ce0eceec330e308cbdeb1ec1e846
Rafae Kamran <abkamran@davidson.edu>
2026-09-16 13:49:14 -0400

    Add heuristic function h2 for Manhattan distance

--- src/astar.py
+++ src/astar.py
@@ -12,11 +12,28 @@ def h1 (board):
   return count
 
 #h2 Manthatten Distance
+#reminder: incase of negative
 
 def h2 (board):
 
   distance = 0
   for i in range (9):
+
+    title = board.state[i]
+    if tile == 0:
+      continue
+
+    currentRow = i // 3
+    currentCol = i % 3
+    
+    goalIndex = GOAL.index(tile)
+    goalRow = goalIndex // 3
+    goalCol = goalIndex % 3
+    
+    distance += abs(currentRow - goalRow)
+    distance += abs(currentCol - goalCol)
+    
+  return distance
     
 
 #A*
